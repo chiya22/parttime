b5b8422f3b0e7a0f87db3fe310b4ebad681c33d7
chiya22 <tomoyay@gmail.com>
2022-03-05 16:03:04 +0900

    終了している人は表示させないよう修正

--- model/yyyymmdds.js
+++ model/yyyymmdds.js
@@ -4,7 +4,7 @@ const logger = log4js.configure("./config/log4js-config.json").getLogger();
 
 const findPKey = async (id_users, yyyymmdd) => {
     try {
-        const retObj = await knex.from("yms").where({id_user: id_users, yyyymmdd: yyyymmdd })
+        const retObj = await knex.from("yyyymmdds").where({id_user: id_users, yyyymmdd: yyyymmdd })
         return retObj;
     } catch(err) {
         throw err;
@@ -16,7 +16,9 @@ const findByYyyymmAndUserid = async (yyyymm, id_users ) => {
         const query = "SELECT * FROM yyyymmdds WHERE yyyymm = '" + yyyymm + "' and id_users = '" + id_users + "' ORDER BY yyyymmdd ASC;"
         logger.info(query);
         const retObj = await knex.raw(query);
+        // Postgres
         return retObj.rows;
+        // MySql
         // return retObj[0];
     } catch(err) {
         throw err;
@@ -25,10 +27,12 @@ const findByYyyymmAndUserid = async (yyyymm, id_users ) => {
 
 const findByYyyymmGroupByUser = async (yyyymm) => {
     try {
-        const query = "SELECT a.id_users, b.name AS name_users FROM yyyymmdds a LEFT OUTER JOIN users b ON a.id_users = b.id  WHERE a.yyyymm = '" + yyyymm + "' GROUP BY id_users, b.name ORDER BY a.id_users asc;"
+        const query = "SELECT a.id_users, b.name AS name_users FROM yyyymmdd a LEFT OUTER JOIN users b ON a.id_users = b.id  WHERE a.yyyymm = '" + yyyymm + "' GROUP BY id_users, b.name ORDER BY a.id_users asc;"
         logger.info(query);
         const retObj = await knex.raw(query);
+        // Postgres
         return retObj.rows;
+        // MySql
         // return retObj[0];
     } catch(err) {
         throw err;
@@ -36,16 +40,17 @@ const findByYyyymmGroupByUser = async (yyyymm) => {
 }
 const findByYyyymmGroupByNoUser = async (yyyymm) => {
     try {
-        const query = "SELECT u.id as id_users, u.name as name_users FROM users u WHERE u.id NOT IN (SELECT ymd.id_users FROM yyyymmdds ymd WHERE ymd.yyyymm = '" + yyyymm + "') AND u.role != 'admin' ORDER BY u.id asc;"
+        const query = "SELECT u.id as id_users, u.name as name_users FROM users u WHERE LEFT(u.ymd_add,6) <= '" + yyyymm + "' AND LEFT(u.ymd_end,6) >= '" + yyyymm + "' AND u.id NOT IN (SELECT ymd.id_users FROM yyyymmdds ymd WHERE ymd.yyyymm = '" + yyyymm + "') AND u.role != 'admin' ORDER BY u.id asc;"
         logger.info(query);
         const retObj = await knex.raw(query);
+        // Postgres
         return retObj.rows;
+        // MySql
         // return retObj[0];
     } catch(err) {
         throw err;
     }
 }
-
 
 const findByYyyymmForDownload = async (yyyymm) => {
     try {
@@ -53,7 +58,9 @@ const findByYyyymmForDownload = async (yyyymm) => {
         const query = "(SELECT a.id_users AS id_users, b.name AS name_users, a.yyyymmdd AS yyyymmdd, a.kubun FROM yyyymmdds a LEFT OUTER JOIN users b ON a.id_users = b.id  WHERE a.yyyymm = '" + yyyymm + "') UNION ALL (SELECT a.id_users AS id_users, b.name AS name_users,'MEMO' as yyyyymmdd, REGEXP_REPLACE(a.memo,'\r|\n|\r\n', '　','g') as kubun FROM memos a LEFT OUTER JOIN users b ON a.id_users = b.id  WHERE a.yyyymm = '" + yyyymm + "') ORDER BY id_users ASC, yyyymmdd ASC"
         logger.info(query);
         const retObj = await knex.raw(query);
+        // Postgres
         return retObj.rows;
+        // MySql
         // return retObj[0];
     } catch(err) {
         throw err;
@@ -65,7 +72,9 @@ const insert = async (inObj) => {
         const query = "insert into yyyymmdds values ('" + inObj.id_users + "','" + inObj.yyyymm + "','" + inObj.yyyymmdd + "','" + inObj.kubun + "','" + inObj.ymd_add + "','" + inObj.id_add + "','" + inObj.ymd_upd + "','" + inObj.id_upd + "')";
         logger.info(query);
         const retObj = await knex.raw(query)
+        // Postgres
         return retObj;
+        // MySql
         // return retObj[0];
     } catch(err) {
         throw err;
@@ -77,7 +86,9 @@ const removeByYyyymmAndUserid = async (yyyymm, id_users) => {
         const query = "delete from yyyymmdds where yyyymm = '" + yyyymm + "' and id_users = '" + id_users + "';"
         logger.info(query);
         const retObj = await knex.raw(query)
+        // Postgres
         return retObj;
+        // MySql
         // return retObj[0];
     } catch(err) {
         throw err;
@@ -85,11 +96,11 @@ const removeByYyyymmAndUserid = async (yyyymm, id_users) => {
 };
 
 module.exports = {
-    findPKey: findPKey,
-    findByYyyymmAndUserid: findByYyyymmAndUserid,
-    findByYyyymmGroupByUser:findByYyyymmGroupByUser,
-    findByYyyymmGroupByNoUser:findByYyyymmGroupByNoUser,
-    findByYyyymmForDownload: findByYyyymmForDownload,
-    insert: insert,
-    removeByYyyymmAndUserid: removeByYyyymmAndUserid,
+    findPKey,
+    findByYyyymmAndUserid,
+    findByYyyymmGroupByUser,
+    findByYyyymmGroupByNoUser,
+    findByYyyymmForDownload,
+    insert,
+    removeByYyyymmAndUserid,
 };
