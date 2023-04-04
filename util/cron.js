d73c59b73a03360564cebee869a63de58ccd000d
chiya22 <10mo8since2020@gmail.com>
2023-04-04 17:10:52 +0900

    LINEグループIDが設定されている場合、LINEグループへメッセージを投稿するよう修正

--- util/cron.js
+++ util/cron.js
@@ -21,43 +21,71 @@ const startcron = () => {
       (async () => {
 
         // 本日のシフト情報を取得
-        const yyyymmdd = tool.getYYYYMMDD(new Date());
+        const yyyymmdd = tool.getYYYYMMDDAfterOneDay(new Date());
         const youbi = tool.getDayKubun(yyyymmdd);
         const youbiStr = youbi==='0'?'日':youbi==='1'?'月':youbi==='2'?'火':youbi==='3'?'水':youbi==='4'?'木':youbi==='5'?'金':'土';
         const yyyymmdd_fix = await yyyymmdds_fix.findByYyyymmdd(yyyymmdd);
 
-        let messagebody;
-        messagebody = `▼ 明日のシフト通知 ▼`;
-        messagebody += `\r\n${yyyymmdd.slice(0,4)}年${yyyymmdd.slice(4,6)}月${yyyymmdd.slice(6,8)}日(${youbiStr})`;
-        messagebody += `\r\n\r\n早番：${yyyymmdd_fix[0].nm_users_haya_1}`;
-        messagebody += yyyymmdd_fix[0].nm_users_haya_2?yyyymmdd_fix[0].nm_users_haya_2:"";
-        messagebody += `\r\n遅番：${yyyymmdd_fix[0].nm_users_oso_1}`;
-        messagebody += yyyymmdd_fix[0].nm_users_oso_2?yyyymmdd_fix[0].nm_users_oso_2:"";
-        messagebody += '\r\n\r\n出勤よろしくお願いしますロボ！';
-
-        const message = {
-          type: 'text',
-          text: messagebody
-        };
-
-        // LINEコンテンツを生成
-        const client = new line.Client({
-          channelAccessToken: process.env.LINE_ACCESS_TOKEN
-        });
-        
-        // LINE送信
-        await client.pushMessage(process.env.LINE_SHAIN_GROUP, message)
-          .then(() => {
-            logger.info("メッセージの送信に成功しました。");
-          })
-          .catch((err) => {
-            logger.info("メッセージの送信に失敗しました。：" + err);
-          });
+        // 早番１
+        if (yyyymmdd_fix[0].id_line_group_haya_1) {
+          let messagebody;
+          messagebody = `▼ 明日のシフト通知 ▼`;
+          messagebody += `\r\n${yyyymmdd_fix[0].nm_users_haya_1}さんは明日${yyyymmdd.slice(4,6)}月${yyyymmdd.slice(6,8)}日(${youbiStr})の早番です。`;
+          messagebody += `\r\n忘れずに出勤お願いしますロボ`;
+          await sendLineMessage(messagebody, yyyymmdd_fix[0].id_line_group_haya_1)
+        }
+        // 早番２
+        if (yyyymmdd_fix[0].id_line_group_haya_2) {
+          let messagebody;
+          messagebody = `▼ 明日のシフト通知 ▼`;
+          messagebody += `\r\n${yyyymmdd_fix[0].nm_users_haya_2}さんは明日${yyyymmdd.slice(4,6)}月${yyyymmdd.slice(6,8)}日(${youbiStr})の早番です。`;
+          messagebody += `\r\n忘れずに出勤お願いしますロボ`;
+          await sendLineMessage(messagebody, yyyymmdd_fix[0].id_line_group_haya_2)
+        }
+        // 遅番１
+        if (yyyymmdd_fix[0].id_line_group_oso_1) {
+          let messagebody;
+          messagebody = `▼ 明日のシフト通知 ▼`;
+          messagebody += `\r\n${yyyymmdd_fix[0].nm_users_oso_1}さんは明日${yyyymmdd.slice(4,6)}月${yyyymmdd.slice(6,8)}日(${youbiStr})の遅番です。`;
+          messagebody += `\r\n忘れずに出勤お願いしますロボ`;
+          await sendLineMessage(messagebody, yyyymmdd_fix[0].id_line_group_oso_1)
+        }
+        // 遅番２
+        if (yyyymmdd_fix[0].id_line_group_oso_2) {
+          let messagebody;
+          messagebody = `▼ 明日のシフト通知 ▼`;
+          messagebody += `\r\n${yyyymmdd_fix[0].nm_users_oso_2}さんは明日${yyyymmdd.slice(4,6)}月${yyyymmdd.slice(6,8)}日(${youbiStr})の遅番です。`;
+          messagebody += `\r\n忘れずに出勤お願いしますロボ`;
+          await sendLineMessage(messagebody, yyyymmdd_fix[0].id_line_group_oso_2)
+        }
 
       })();
-
     });
   }
+};
+
+const sendLineMessage = (messagebody, sendToId) => {
+  (async () => {
+    const message = {
+      type: 'text',
+      text: messagebody
+    };
+  
+    // LINEコンテンツを生成
+    const client = new line.Client({
+      channelAccessToken: process.env.LINE_ACCESS_TOKEN
+    });
+    
+    // LINE送信
+    // await client.pushMessage(process.env.LINE_SHAIN_GROUP, message)
+    await client.pushMessage(sendToId, message)
+      .then(() => {
+        logger.info("メッセージの送信に成功しました。");
+      })
+      .catch((err) => {
+        logger.info("メッセージの送信に失敗しました。：" + err);
+      });
+  })()
 };
 
 module.exports = {
